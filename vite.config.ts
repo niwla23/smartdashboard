fa6290581126a5b49a47e1658f9d42c229f087cd
niwla23 <46248939+niwla23@users.noreply.github.com>
2023-04-16 15:31:51 +0200

    add hashes to built files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+const hash = Math.floor(Math.random() * 90000) + 10000
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'es2015'
-  }
+    target: "es2015",
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].` + hash + `.js`,
+        chunkFileNames: `[name].` + hash + `.js`,
+        assetFileNames: `[name].` + hash + `.[ext]`,
+      },
+    },
+  },
 })
